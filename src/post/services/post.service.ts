6f577c682bde03edf495e66214fcc0e3fa9452e4
ValentinZoia <valentinzoia@gmail.com>
2025-09-20 21:07:50 -0300

    feat(create new methods)

--- src/post/services/post.service.ts
+++ src/post/services/post.service.ts
@@ -3,12 +3,15 @@ import { Post } from '../entities/post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePostDto } from '../dtos/post.dto';
+import { Author } from 'src/authors/entities/author.entity';
+import { AuthorsService } from 'src/authors/authors.service';
 
 @Injectable()
 export class PostService {
   constructor(
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>,
+    private readonly authorsService: AuthorsService,
   ) {}
 
   async findAll(): Promise<Post[]> {
@@ -26,6 +29,39 @@ export class PostService {
       return await this.postRepository.save(newPost);
     } catch (error) {
       console.error(error);
+      throw new HttpException(error, 500);
+    }
+  }
+
+  async findOneById(id: number): Promise<Post> {
+    try {
+      const post = await this.postRepository
+        .createQueryBuilder('post')
+        .where({ id })
+        .getOne();
+
+      if (!post) {
+        throw new HttpException('Post not found', 404);
+      }
+
+      return post;
+    } catch (error) {
+      throw new HttpException(error, 500);
+    }
+  }
+
+  async getAuthors(): Promise<Author[]> {
+    try {
+      return await this.authorsService.findAll();
+    } catch (error) {
+      throw new HttpException(error, 500);
+    }
+  }
+  async getAuthorById(id: number): Promise<Author> {
+    try {
+      const author = await this.authorsService.findOne(id);
+      return author;
+    } catch (error) {
       throw new HttpException(error, 500);
     }
   }
